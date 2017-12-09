b552dc55bd86bf7e744934e043b37f45859e599b
agamaru <agamaru@gmail.com>
2017-12-09 11:59:32 +0100

    Incluidos algunos servicios de ejemplo generados manualmente

--- src/AppBundle/Controller/ServicioController.php
+++ src/AppBundle/Controller/ServicioController.php
@@ -12,6 +12,15 @@ class ServicioController extends Controller
      */
     public function listadoAction()
     {
-        return $this->render('servicio/listado.html.twig');
+        $servicios = [
+            ['titulo' => 'Título uno', 'descripcion' => 'Descripción uno', 'autor' => 'Alonso Quijano', 'fecha_publicacion' => new \DateTime('2017-12-05')],
+            ['titulo' => 'Título dos', 'descripcion' => 'Descripción dos', 'autor' => 'Sancho Panza', 'fecha_publicacion' => new \DateTime('2017-12-06')],
+            ['titulo' => 'Título tres', 'descripcion' => 'Descripción tres', 'autor' => 'Dulcinea del Toboso', 'fecha_publicacion' => new \DateTime('2017-12-07')],
+            ['titulo' => 'Título cuatro', 'descripcion' => 'Descripción cuatro', 'autor' => 'Ginés de Pasamonte', 'fecha_publicacion' => new \DateTime('2017-12-08')]
+        ];
+
+        return $this->render('servicio/listado.html.twig', [
+            'servicios' => $servicios
+        ]);
     }
 }
